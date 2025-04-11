92cfc148fec971173a9ab26d31fcf33731ab942b
TuanOnta <mdz54dzakiarta@gmail.com>
2025-04-11 08:50:35 +0700

    feat: add api for verify and refresh token

--- app/src/main/java/com/example/purrytify/network/ApiService.kt
+++ app/src/main/java/com/example/purrytify/network/ApiService.kt
@@ -3,6 +3,7 @@ package com.example.purrytify.network
 import com.example.purrytify.model.LoginRequest
 import com.example.purrytify.model.LoginResponse
 import com.example.purrytify.model.UserProfile
+import com.example.purrytify.model.RefreshTokenRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -13,10 +14,17 @@ interface ApiService {
     @POST("api/login")
     suspend fun login(@Body request: LoginRequest): Response<LoginResponse>
 
-    // Mendapatkan data profile pengguna
-    // Header "Authorization" harus berisi "Bearer {token}"
     @GET("api/profile")
     suspend fun getUserProfile(
-    @Header("Authorization") authToken: String
     ): Response<UserProfile>
+
+    @GET("api/verify-token")
+    suspend fun verifyToken(
+        @Header("Authorization") authHeader: String
+    ): Response<Unit>
+
+    @POST("api/refresh-token")
+    suspend fun refreshToken(
+        @Body request: RefreshTokenRequest
+    ): Response<LoginResponse>
 }
