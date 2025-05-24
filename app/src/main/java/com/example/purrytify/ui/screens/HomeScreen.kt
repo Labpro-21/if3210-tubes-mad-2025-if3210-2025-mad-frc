171094587e33f32199ddfbd0f16aeba8b7acd9e4
Axel Santadi <axelsantadi@gmail.com>
2025-05-24 14:12:26 +0700

    refactor: delete uneccessary import

--- app/src/main/java/com/example/purrytify/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/purrytify/ui/screens/HomeScreen.kt
@@ -15,14 +15,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Download
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,44 +41,27 @@ import com.example.purrytify.ui.navBar.VerticalNavBar
 import com.example.purrytify.viewmodel.OnlineSongViewModel
 import com.example.purrytify.viewmodel.PlayerViewModel
 import com.example.purrytify.viewmodel.SongViewModel
-import com.example.purrytify.ui.navBar.BottomNavBar
 import android.content.pm.ActivityInfo
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import com.example.purrytify.ui.navBar.VerticalNavBar
 import android.content.res.Configuration
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.purrytify.network.ApiService
 import com.example.purrytify.network.RetrofitClient
 import com.example.purrytify.utils.TokenManager
 import com.example.purrytify.utils.SessionManager
 import com.example.purrytify.viewmodel.OnlineSongViewModelFactory
-import com.example.purrytify.viewmodel.ProfileViewModel
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.ui.graphics.Brush
-
-
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.ModalBottomSheet
 import java.util.Date
-
 import com.example.purrytify.ui.components.SongSettingsModal
 import com.example.purrytify.ui.components.TopModalBottomSheet
 
